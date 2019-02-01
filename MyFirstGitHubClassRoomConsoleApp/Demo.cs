8f4536da8f115898f15e22e1ae5371d4b22d59b6
Jacob Larsen <jacob.larsen6401@hotmail.dk>
2019-02-01 11:35:28 +0100

    Tilføjede contructor

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,16 @@
+﻿namespace MyFirstGitHubClassRoomConsoleApp
+{
+    public class Demo
+    {
+        private string _skole;
+        public string _skolevej;
+        public int _skoleid;
+
+        public Demo(string skole, string skoleVej, int skoleID)
+        {
+            _skole = skole;
+            _skolevej = skoleVej;
+            _skoleid = skoleID;
+        }
+    }
+}
